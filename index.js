95c9d346b37b3d0e7b733fe3570f2961d4992c29
ppajer <peter.p.pajer@gmail.com>
2020-08-06 13:39:27 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+module.exports = class DateTime {
+  
+  constructor(initialValue) {
+    this.date = new Date(initialValue);
+  }
+  
+  getDay() {
+    return this.date.getDate();
+  }
+  
+  getMonth() {
+    return this.date.getMonth() + 1;
+  }
+  
+  getYear() {
+    return this.date.getFullYear();
+  }
+  
+  getMillisecond() {
+    return this.date.getMilliseconds();
+  }
+  
+  getSecond() {
+    return this.date.getSeconds();
+  }
+  
+  getMinute() {
+    return this.date.getMinutes();
+  }
+  
+  getHour() {
+    return this.date.getHours();
+  }
+  
+  getTime(highPrecision = false) {
+    let h = this.getHour();
+    let m = this.getMinute();
+    let s = this.getSecond();
+    let ms = this.getMillisecond();
+    
+    return h+':'+m+':'+s+(highPrecision ? '.'+ms : '');
+  }
+  
+  format(format) {
+    // TODO
+  }
+  
+  toSQL() {
+    // TODO
+  }
+}
